6268868ef44b98efe39415de1eefbad0c81fe567
Anaswara T Rajan <anaswaratrajan@gmail.com>
2025-06-01 11:01:08 +0530

    candidate email unique and update gender choices

--- ats/models.py
+++ ats/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Candidate(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
     name = models.CharField(max_length=150)
     age = models.IntegerField()
-    gender = models.CharField(choices=[('M', 'Male'), ('F', 'Female')], max_length=1)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=15)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=1, blank=True, null=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=10)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
